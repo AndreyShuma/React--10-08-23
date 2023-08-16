2db804c7ae51f5b46d71882ecfa14fbcf6122e30
AndreyShuma <0662389612a@gmail.com>
2023-08-16 14:25:01 +0300

    16-08-23

--- project/src/App/index.js
+++ project/src/App/index.js
@@ -1,10 +1,30 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 import logo from '../logo.svg';
 import './App.css';
 
 
 function App() {
   const [left, setLeft] = useState(0);
+  const [start, setStart] = useState('');
+  
+  console.log('початок');
+
+useEffect(() => {
+  const fetchData = async () => {
+    const result = await axios.get('https://swapi.dev/api/people/1');
+    console.log('result >>>', result.data.name);
+    console.log('start>>>', start);
+    setStart(result.data.name);
+    console.log('end>>>', start);
+  };
+
+  fetchData();
+}, []);
+
+  console.log('кінець start>>>', start);
+
+
   const handlerclick = () => {
     setLeft(left + 1)
   }
@@ -22,10 +42,11 @@ function App() {
           rel="noopener noreferrer"
         >
           {left ? <>True--</>  : <>False--</>}
-          {/* { console.log('False')  console.log('True')} */}
+
           Learn React {left}
         </a>
         <button onClick={handlerclick}>Click</button>
+        <p>Тут знаходиться ім'я з сайту swapi.dev -----{start}</p>
       </header>
     </div>
   );
